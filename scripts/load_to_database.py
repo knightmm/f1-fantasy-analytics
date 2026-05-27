188cb66d57814a25d2c9f56040c3a301909d0e2b
Miranda Knight <knightm@tcd.ie>
2026-05-27 12:28:31 +0200

    Add run_pipeline to centralize orchestration. Add context management to SQL connection in load_to_database. Update .gitignore.

--- scripts/load_to_database.py
+++ scripts/load_to_database.py
@@ -1,60 +1,62 @@
 import sqlite3
 import pandas as pd
 import os
+
 from src.utils.database import load_csvs_to_table
 
-# Define Paths
-DATABASE_PATH = os.path.join("data", "f1_fantasy.db")
-PROCESSED_DIR = os.path.join("data", "processed")
+def main():
+    
+    # Define Paths
+    DATABASE_PATH = os.path.join("data", "f1_fantasy.db")
+    PROCESSED_DIR = os.path.join("data", "processed")
 
-# Connect to SQLite
-con = sqlite3.connect(DATABASE_PATH)
+    # Context Manager to connect to SQLite
+    with sqlite3.connect(DATABASE_PATH) as con:
 
-load_csvs_to_table(
-    PROCESSED_DIR,
-    "constructors_race_",
-    "constructors_race_snapshots",
-    con,
-)
+        load_csvs_to_table(
+            PROCESSED_DIR,
+            "constructors_race_",
+            "constructors_race_snapshots",
+            con,
+        )
 
-load_csvs_to_table(
-    PROCESSED_DIR,
-    "drivers_race_",
-    "drivers_race_snapshots",
-    con,
-)
+        load_csvs_to_table(
+            PROCESSED_DIR,
+            "drivers_race_",
+            "drivers_race_snapshots",
+            con,
+        )
 
-load_csvs_to_table(
-    PROCESSED_DIR,
-    "asset_prices_race_",
-    "asset_price_snapshots",
-    con,
-)
+        load_csvs_to_table(
+            PROCESSED_DIR,
+            "asset_prices_race_",
+            "asset_price_snapshots",
+            con,
+        )
 
-load_csvs_to_table(
-    PROCESSED_DIR,
-    "league_standings_race_",
-    "league_standings",
-    con,
-)
+        load_csvs_to_table(
+            PROCESSED_DIR,
+            "league_standings_race_",
+            "league_standings",
+            con,
+        )
 
-load_csvs_to_table(
-    PROCESSED_DIR,
-    "league_team_assets_race_",
-    "league_team_assets",
-    con,
-)
+        load_csvs_to_table(
+            PROCESSED_DIR,
+            "league_team_assets_race_",
+            "league_team_assets",
+            con,
+        )
 
-# Write Race Data file to DB
-races_filepath = os.path.join("data", "reference", "races_2026.csv")
+        # Write Race Data file to DB
+        races_filepath = os.path.join("data", "reference", "races_2026.csv")
 
-races_df = pd.read_csv(races_filepath)
-races_df["race_date"] = pd.to_datetime(races_df["race_date"], utc=True)
+        races_df = pd.read_csv(races_filepath)
+        races_df["race_date"] = pd.to_datetime(races_df["race_date"], utc=True)
 
-races_df.to_sql("races", con, if_exists="replace", index=False)
+        races_df.to_sql("races", con, if_exists="replace", index=False)
 
+        print("Database load complete.")
 
-# Close Connection
-con.close()
-
-print("Database load complete.")
+if __name__ == "__main__":
+    main()
--- sql/create_marts.sql
+++
@@ -1,5 +0,0 @@
-DROP TABLE IF EXISTS mart_driver_value;
-
-CREATE TABLE mart_driver_value AS
-SELECT *
-FROM drivers_race_snapshots;
